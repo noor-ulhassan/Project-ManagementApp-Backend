258b11125b5ab9b666ebcc63fbbaa7f85da7db42
Noor <thenoorulhassan.dev@gmail.com>
2026-02-23 23:33:47 +0500

    CORS configurations

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//basic configurations--------------
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configuration----------------
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+export default app;
