b6cf0c678c56ac3e5ba909425bb12ca9c8b94d32
Noor <thenoorulhassan.dev@gmail.com>
2026-02-27 03:27:34 +0500

    User Register

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { emailVerificationMaingenContent, sendEmail } from "../utils/mail.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Failed to generate tokens");
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "Username or email already exists");
+  }
+
+  const user = await User.create({
+    email,
+    username,
+    password,
+    isEmailVerified: false,
+  });
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email",
+    mailgenContent: emailVerificationMaingenContent(
+      user.username,
+      `${req.protocol}://${req.get("host")}/api/auth/verify-email?token=${unHashedToken}`,
+    ),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Failed to create user");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(200, "User registered successfully", createdUser));
+});
+
+export { registerUser };
